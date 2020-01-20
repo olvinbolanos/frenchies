f5bba7e72d6407ecc5ece88bc3ce3285f5da2ebe
Olvin <olvin.bolanos@yahoo.com>
2020-01-20 14:03:03 -0800

    pushing state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import { Route, Switch } from 'react-router-dom'
+import Pages from './Pages'
+// import createApp from '@shopify/app-bridge'
+// const app = createApp({
+//   apiKey:'ff86ea300fec2db97e9ac97f94ae6cd1',
+//   shopOrigin: 'frenchiepuppies.myshopify.com'
+// });
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      id: '',
+      username: ''
+    }
+  }
+
+  render() {
+    return (
+      <main>
+        <Switch>
+          <Route exact path='/' render={(props) => <Pages />} />
+        </Switch>
+      </main>
+    );
+  }
 }
 
 export default App;
