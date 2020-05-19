b45ab2b4f75e7c87b865001da275b39a06917114
Olvin <olvin.bolanos@yahoo.com>
2020-05-18 21:52:32 -0700

    applying typescript in file

---
+++ pages/login.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useApolloClient, useMutation } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
+import { LoginForm, Loading } from '../components';
+import ApolloClient from 'apollo-client';
+import * as LoginTypes from './__generated__/login';
+
+export const LOGIN_USER = gql`
+  mutation Login($email: String!) {
+    login(email: $email)
+  }
+`;
+
+export default function Login() {
+  const client: ApolloClient<any> = useApolloClient();
+  const [login, { loading, error }] = useMutation<LoginTypes.Login,LoginTypes.loginVariables>(
+      LOGIN_USER,
+      {
+        onCompleted({ login }) {
+          localStorage.setItem('token', login as string);
+          client.writeData({ data: {isLoggedIn: true } });
+        }
+      }
+  );
+}
